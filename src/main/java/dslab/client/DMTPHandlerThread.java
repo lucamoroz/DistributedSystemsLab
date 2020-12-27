8c5ff9a36f8ef5727b5c30f4a2cb8635e22f0248
SirChronus <chronus@prefix.moe>
2020-12-28 00:48:56 +0100

    Added msg send command for client

---
+++ src/main/java/dslab/client/DMTPHandlerThread.java
@@ -0,0 +1,106 @@
+package dslab.client;
+
+import dslab.protocols.dmtp.DMTPException;
+import dslab.protocols.dmtp.Email;
+import dslab.protocols.dmtp.client.DMTPClientHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+
+public class DMTPHandlerThread extends Thread {
+    private final Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private DMTPClientHandler handler;
+    private final BlockingQueue<Email> blockingQueue;
+
+    public DMTPHandlerThread(Socket socket, BlockingQueue<Email> blockingQueue) {
+        this.socket = socket;
+        this.blockingQueue = blockingQueue;
+    }
+
+    public boolean init() {
+        // Create resources for DMTP handler
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("Could not create reader and writer for client DMTP handler thread: " + e.getMessage());
+            return false;
+        }
+
+        handler = new DMTPClientHandler(socket, reader, writer);
+
+        // Initialize handler and clean up on error
+        try {
+            handler.init();
+        } catch (IOException e) {
+            System.out.println("DMTP handler init threw IOException: " + e.getMessage());
+            closeResources();
+            return false;
+        } catch (DMTPException e) {
+            System.out.println("DMTP handler init threw DMTPException: " + e.getMessage());
+            closeResources();
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+
+            try {
+                Email email = blockingQueue.take();
+
+                // Try to send email taken from blocking queue
+                try {
+                    // We only need to print out the first recipient since there can only be one defined in the msg command
+                    handler.sendEmail(email, recipients -> System.out.println("error unknown recipient: " + recipients.get(0)));
+                } catch (IOException | DMTPException e) {
+                    System.out.printf("error sending '%s': %s%n", email.subject, e.getMessage());
+                    continue;
+                }
+
+                System.out.println("ok");
+            } catch (InterruptedException e) {
+                // In case of interruption, email will be lost.
+                // A better approach could permanently store email data and remove that data only after an email has
+                // been processed.
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // cleanup all used resources
+        try {
+            handler.close();
+        } catch (IOException | DMTPException e) {
+            System.out.println("Error closing DMTP connection: " + e.getMessage());
+        }
+
+        closeResources();
+    }
+
+    private void closeResources() {
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (IOException exception) { }
+        }
+
+        if (writer != null) {
+            writer.close();
+        }
+
+        if (socket != null && !socket.isClosed()) {
+            try {
+                socket.close();
+            } catch (IOException exception) { }
+        }
+    }
+}
--- src/main/java/dslab/client/MessageClient.java
+++ src/main/java/dslab/client/MessageClient.java
@@ -1,12 +1,29 @@
 package dslab.client;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.UncheckedIOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
+import at.ac.tuwien.dsg.orvell.Shell;
+import at.ac.tuwien.dsg.orvell.StopShellException;
+import at.ac.tuwien.dsg.orvell.annotation.Command;
 import dslab.ComponentFactory;
+import dslab.protocols.dmtp.Email;
 import dslab.util.Config;
 
 public class MessageClient implements IMessageClient, Runnable {
+    final private Config config;
+    final private Shell shell;
+    private Socket dmtpSocket;
+    private Socket dmapSocket;
+    private DMTPHandlerThread dmtpHandler;
+    private final BlockingQueue<Email> blockingQueue;
+    private final String sender;
 
     /**
      * Creates a new client instance.
@@ -17,37 +34,107 @@ public class MessageClient implements IMessageClient, Runnable {
      * @param out the output stream to write console output to
      */
     public MessageClient(String componentId, Config config, InputStream in, PrintStream out) {
+        this.config = config;
+        sender = config.getString("transfer.email");
 
+        shell = new Shell(in, out);
+        shell.register(this);
+        shell.setPrompt(componentId + "> ");
+
+        blockingQueue = new ArrayBlockingQueue<>(40);
     }
 
     @Override
     public void run() {
+        try {
+            dmtpSocket = new Socket(config.getString("transfer.host"), config.getInt("transfer.port"));
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error while creating client DMTP socket", e);
+        }
 
+        dmtpHandler = new DMTPHandlerThread(dmtpSocket, blockingQueue);
+        if (!dmtpHandler.init()) {
+            return;
+        }
+        dmtpHandler.start();
+
+        /*try {
+            dmapSocket = new Socket(config.getString("mailbox.host"), config.getInt("mailbox.port"));
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error while creating client DMAP socket", e);
+        }*/
+
+        shell.run();
     }
 
     @Override
+    @Command
     public void inbox() {
 
     }
 
     @Override
+    @Command
     public void delete(String id) {
 
     }
 
     @Override
+    @Command
     public void verify(String id) {
 
     }
 
     @Override
+    @Command
     public void msg(String to, String subject, String data) {
+        // validate address format
+        if (!Email.isValidAddress(to)) {
+            shell.out().printf("error given recipient '%s' is not a valid mail address%n", to);
+            return;
+        }
 
+        ArrayList<String> recipients = new ArrayList<>();
+        recipients.add(to);
+        Email email = new Email(sender, recipients, subject, data);
+        blockingQueue.add(email);
     }
 
     @Override
+    @Command
     public void shutdown() {
+        // quit DMTP handler thread
+        if (dmtpHandler != null) {
+            dmtpHandler.interrupt();
+            try {
+                // wait for thread to quit
+                dmtpHandler.join();
+            } catch (InterruptedException e) {
+                // noop
+            }
+        }
 
+        // close DMTP socket
+        if (dmtpSocket != null && !dmtpSocket.isClosed()) {
+            try {
+                dmtpSocket.close();
+            } catch (IOException e) {
+                shell.out().println("Error while closing DMTP socket: " + e.getMessage());
+            }
+        }
+
+        // close DMAP socket
+        if (dmapSocket != null && !dmapSocket.isClosed()) {
+            try {
+                dmapSocket.close();
+            } catch (IOException e) {
+                shell.out().println("Error while closing DMAP socket: " + e.getMessage());
+            }
+        }
+
+        shell.out().println("Bye bye");
+
+        throw new StopShellException();
     }
 
     public static void main(String[] args) throws Exception {
